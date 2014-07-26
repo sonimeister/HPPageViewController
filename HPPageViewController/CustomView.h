7b7de1be7e6c7085c9d5b118d18a207235e49e8e
lizixroy <lizixuanroy@hotmail.com>
2014-07-26 10:16:00 -0400

    Updated

---
+++ HPPageViewController/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  ScrollViewTest
+//
+//  Created by Zixuan Li on 7/25/14.
+//  Copyright (c) 2014 Mallocu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
